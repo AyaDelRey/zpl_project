645f1536b7814d95cf2b680bb0b1e7de5c57ea25
Marwa Chanu <m.chanu@interface3.be>
2025-06-17 21:18:49 +0200

    module template

---
+++ addons/zpl_custo/_manifest_.py
@@ -0,0 +1,18 @@
+{
+    'name': "My ZPL Label Report",
+    'version': "1.0",
+    'summary': "Report template for ZPL label printing",
+    'category': 'Reporting',
+    'author': "TonNom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base_report_to_label_printer',
+        'base_report_to_printer',
+    ],
+    'data': [
+        'views/report_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
